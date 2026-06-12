357ce0e9643140251cdd0356d2597c149631147e
ChatAgent <chatagent@example.com>
2026-06-12 20:35:13 +0800

    fix: upgrade Kotlin to 2.0.21, use Compose compiler plugin
    - Kotlin 1.9.24 → 2.0.21
    - 添加 org.jetbrains.kotlin.plugin.compose 插件
    - 移除 composeOptions.kotlinCompilerExtensionVersion

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,10 @@ pluginManagement {
     }
     plugins {
         id("com.android.application") version "8.7.3"
-        id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("org.jetbrains.kotlin.android") version "2.0.21"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
         id("com.google.dagger.hilt.android") version "2.50"
-        id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
     }
 }
 
