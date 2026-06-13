d9580469d41c98cffde8a3f0716c5bfdb8dd73a5
ChatAgent <chatagent@example.com>
2026-06-14 03:46:40 +0800

    debug: 添加崩溃堆栈捕获 (写入 crash_log.txt)

--- app/src/main/java/com/chatagent/ChatAgentApp.kt
+++ app/src/main/java/com/chatagent/ChatAgentApp.kt
@@ -4,4 +4,21 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ChatAgentApp : Application()
+class ChatAgentApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val crashFile = java.io.File(filesDir, "crash_log.txt")
+        val prevHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            try {
+                val sw = java.io.StringWriter()
+                val pw = java.io.PrintWriter(sw)
+                throwable.printStackTrace(pw)
+                pw.flush()
+                crashFile.writeText(sw.toString())
+            } catch (_: Exception) {}
+            prevHandler?.uncaughtException(thread, throwable)
+        }
+    }
+}
